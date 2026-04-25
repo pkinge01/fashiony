ba2c6b0d445224a6d71a30129b3848a952963aff
pkinge01 <51260439+pkinge01@users.noreply.github.com>
2026-04-25 07:50:42 +0530

    Update product.js

--- js/product.js
+++ js/product.js
@@ -49,7 +49,7 @@ const displayProducts = (products, center) => {
   center.innerHTML = display;
 };
 
-// Filtering / sorting products 
+// Filtering products 
 const catContainer = document.querySelector(".sort-category");
 const filterBtns = [...document.querySelectorAll(".filter-btn")];
 
